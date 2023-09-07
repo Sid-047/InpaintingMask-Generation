917db3ff8102bf0f2b99c631328282d353381103
sid <sid@adoltech.com>
2023-09-07 17:23:40 +0530

    Updated Code

--- maskCanvas.py
+++ maskCanvas.py
@@ -10,50 +10,41 @@ from colorama import Fore, Style
 
 tk = Tk()
 os.chdir("testImages")
-img = "1.jpg"
+inImg = "1.jpg"
 
 tk.title("Mask Canvas")
 c=Canvas(tk, width=1024, height=1024, bg='black')
 c.pack(expand=NO)
 
-bg = PhotoImage(file = img)
+bg = ImageTk.PhotoImage(Image.open(inImg))
 bg_img = c.create_image(0, 0, anchor=NW, image=bg)
 
 def fig(event):
     x1,y1=(event.x-15),(event.y-15)
     x2,y2=(event.x+15),(event.y+15)
-    c.create_oval(x1, y1, x2, y2, fill='white', outline='white')
+    c.create_oval(x1, y1, x2, y2, fill='white', outline='white', tags='overlay')
 
 def del_con(event):
     tk.title("Mask Canvas")
     plt.close('all')
-    c.delete('all')
-    bg_img
+    c.delete('overlay')
 
-'''def img_con(event):
-    global a
-    global ar
-    m = 3
-    x, y = c.winfo_rootx(), c.winfo_rooty()
-    w, h = c.winfo_width(), c.winfo_height()
-    pyautogui.screenshot(str(a)+'.jpg', region=(x+m, y+m, w-m*2, h-m*2))
-    img=cv2.imread(str(a)+'.jpg', cv2.IMREAD_GRAYSCALE)
-    print(Fore.BLUE+Style.BRIGHT+str(np.shape(img))+Fore.RESET)
-    img_=cv2.resize(img, dsize=(28,28), interpolation=cv2.INTER_AREA)
-    print(np.shape(img_))
-    cv2.imwrite(str(a)+'.jpg', img_)
-    print("---->"+Fore.BLUE+Style.BRIGHT+str(a)+'.jpg'+Fore.RESET)
-    ar=np.asarray(img_)
-    plt.figure(figsize=[10,5])
-    plt.subplot(121)
-    plt.imshow(ar, cmap='gray')
+def img_con(event):
+    c.delete(bg_img)
+    c.update()
+    outImg = '_Mask.'.join(inImg.split('.'))
+    fileInfo = outImg.split('.')
+    print("---->", outImg)
+    c.postscript(file = fileInfo[0]+'.ps', colormode = 'gray')
+    img  = Image.open(fileInfo[0]+'.ps').convert('L')
+    img_ar = np.array(img)
+    print('----------------->',img_ar.shape)
+    plt.imshow(img_ar, cmap='gray')
     plt.show(block=False)
-    a+=1
-    c.delete('all')
-    tk.title("Digit - "+str(a)[-1])'''
+    img.save(outImg, fileInfo[1].upper())
 
 c.bind('<B1-Motion>', fig)
 c.bind('<Button-3>', del_con)
-#c.bind('<Button-2>', img_con)
+c.bind('<Button-2>', img_con)
 
 tk.mainloop()
